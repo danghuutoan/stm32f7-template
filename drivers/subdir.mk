00282eb06a88bae7706a23d70924f9ba064439e9
danghuutoan <danghuutoan@gmail.com>
2018-11-03 17:57:40 +0700

    merging with the rtos branch

--- drivers/subdir.mk
+++ drivers/subdir.mk
@@ -1,7 +1,7 @@
 # DRIVER
 DRIVER_DIR = drivers
 # SRC
-CMSIS_SRCS+= CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx.c
+# CMSIS_SRCS+= CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx.c
 
 # INC
 CMSIS_INC += CMSIS/Include \
@@ -23,6 +23,3 @@ HAL_INCS += STM32F7xx_HAL_Driver/Inc
 DRIVER_INC = $(addprefix $(DRIVER_DIR)/, $(CMSIS_INC) $(HAL_INCS) $(BSP_INC))
 DRIVER_SRCS = $(addprefix $(DRIVER_DIR)/, $(CMSIS_SRCS) $(BSP_SRCS) $(HAL_SRCS))
 DRIVER_OBJS = $(addsuffix .o, $(basename $(DRIVER_SRCS)))
-
-$(info    DRIVER_SRCS is $(DRIVER_SRCS))
-$(info    DRIVER_OBJS is $(DRIVER_OBJS))
